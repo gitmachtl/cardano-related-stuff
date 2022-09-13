769f3137c39ae2e8d49734ec867a2de4d647a3df
gitmachtl <martin@machtl.com>
2022-09-13 20:34:27 +0200

    New version: cardano-signer 1.2.0

--- cardano-signer/src/cardano-signer.js
+++ cardano-signer/src/cardano-signer.js
@@ -1,31 +1,30 @@
 const appname = "cardano-signer"
-const version = "1.1.0"
+const version = "1.2.0"
 
 const CardanoWasm = require("@emurgo/cardano-serialization-lib-nodejs")
+const fs = require("fs");
 
 const regExp = /^[0-9a-fA-F]+$/;
 
 function showUsage(){
         console.log(``)
-        console.log(`Usage Signing:`)
+        console.log(`Signing a hexstring(data):`)
         console.log(``)
-        console.log(`   ${appname} sign <data(hex)> <secret_key(hex)>`)
-        console.log(``)
-        console.log(`   Output: signature(hex) + public_key(hex)`)
-        console.log(``)
-        console.log(``)
-        console.log(`Usage Verify:`)
-        console.log(``)
-        console.log(`   ${appname} verify <data(hex)> <signature_to_verify(hex)> <public_key(hex)>`)
-        console.log(``)
-        console.log(`   Output: true(exitcode 0) or false(exitcode 1)`)
+        console.log(`	Syntax: ${appname} sign <data_hex> <secretKey_file|secretKey_hex>`)
+        console.log(`	Output: signature_hex + publicKey_hex`)
         console.log(``)
         console.log(``)
-        console.log(`Info:`)
-        console.log(`   Brought to you by: https://github.com/gitmachtl \/\/ Cardano SPO Scripts \/\/ ATADA Stakepools Austria`)
+        console.log(`Verifying a hexstring(data)+signature+publicKey:`)
+        console.log(``)
+        console.log(`	Syntax: ${appname} verify <data_hex> <signature_hex> <publicKey_file|publicKey_hex>`)
+        console.log(`	Output: true(exitcode 0) or false(exitcode 1)`)
+        console.log(``)
+        console.log(``)
+        console.log(`Info: https://github.com/gitmachtl (Cardano SPO Scripts \/\/ ATADA Stakepools Austria)`)
         console.log(``)
         process.exit(1);
 }
+
 
 function trimString(s){
         s = s.replace(/(^\s*)|(\s*$)/gi,"");    //exclude start and end white-space
@@ -33,27 +32,100 @@ function trimString(s){
         return s;
 }
 
+
+function readKey2hex(key,type) { //reads a standard-cardano-skey/vkey-file-json or a direct hex entry // returns a hexstring of the key
+
+	var key_hex = "";
+
+	switch (type) {
+
+		case "secret": //convert a secret key into a hex string
+
+			// try to use the parameter as a filename for a cardano skey json with a cborHex entry
+			try {
+				const key_json = JSON.parse(fs.readFileSync(key)); //parse the given key as a json file
+				const is_singing_key = key_json.type.toLowerCase().includes('signing') //boolean if the json contains the keyword 'signing' in the type field
+				if ( ! is_singing_key ) { console.log(`Error: The file '${key}' is not a signing/secret key json`); process.exit(1); }
+				key_hex = key_json.cborHex.substring(4).toLowerCase(); //cut off the leading "5820/5840" from the cborHex
+				//check that the given key is a hex string
+				if ( ! regExp.test(key_hex) ) { console.log(`Error: The secret key in file '${key}' entry 'cborHex' is not a valid hex string`); process.exit(1); }
+				return key_hex;
+			} catch (error) {}
+
+			// try to use the parameter as a filename for a bech encoded string in it (typical keyfiles generated via jcli)
+			try {
+				const content = trimString(fs.readFileSync(key,'utf8')); //read the content of the given key from a file
+				try { //try to load it as a bech secret key
+					const tmp_key = CardanoWasm.PrivateKey.from_bech32(content); //temporary key to check about bech32 format
+					key_hex = Buffer.from(tmp_key.as_bytes()).toString('hex');
+				 } catch (error) { console.log(`Error: The content in file '${key}' is not a valid bech secret key`); process.exit(1); }
+				return key_hex;
+			} catch (error) {}
+
+			// try to use the parameter as a direct hex string
+			key_hex = trimString(key.toLowerCase());
+			//check that the given key is a hex string
+			if ( ! regExp.test(key) ) { console.log(`Error: Provided secret key is not a valid hex string, or the file is missing`); process.exit(1); }
+			return key_hex;
+			break;
+
+
+		case "public": //convert a public key into a hex string
+
+			// try to use the parameter as a filename for a cardano vkey json with a cborHex entry
+			try {
+				const key_json = JSON.parse(fs.readFileSync(key)); //parse the given key as a json file
+				const is_verification_key = key_json.type.toLowerCase().includes('verification') //boolean if the json contains the keyword 'verification' in the type field
+				if ( ! is_verification_key ) { console.log(`Error: The file '${key}' is not a verification/public key json`); process.exit(1); }
+				key_hex = key_json.cborHex.substring(4).toLowerCase(); //cut off the leading "5820/5840" from the cborHex
+				//check that the given key is a hex string
+				if ( ! regExp.test(key_hex) ) { console.log(`Error: The public key in file '${key}' entry 'cborHex' is not a valid hex string`); process.exit(1); }
+				return key_hex;
+			} catch (error) {}
+
+			// try to use the parameter as a filename for a bech encoded string in it (typical keyfiles generated via jcli)
+			try {
+				const content = trimString(fs.readFileSync(key,'utf8')); //read the content of the given key from a file
+				try { //try to load it as a bech public key
+					const tmp_key = CardanoWasm.PublicKey.from_bech32(content); //temporary key to check about bech32 format
+					key_hex = Buffer.from(tmp_key.as_bytes()).toString('hex');
+				 } catch (error) { console.log(`Error: The content in file '${key}' is not a valid bech public key`); process.exit(1); }
+				return key_hex;
+			} catch (error) {}
+
+			// try to use the parameter as a direct hex string
+			key_hex = trimString(key.toLowerCase());
+			//check that the given key is a hex string
+			if ( ! regExp.test(key) ) { console.log(`Error: Provided public key is not a valid hex string, or the file is missing`); process.exit(1); }
+			return key_hex;
+			break;
+
+	} //switch (type)
+
+}
+
+
 // MAIN
 //
 // first parameter -> workMode: sign or verify
 //
 // workMode: sign
 //second parameter -> hexdata that should be signed
-// third parameter -> signing key in hex format
+// third parameter -> signing key in hex format or the path to a file (json/txt)
 //          output -> signed data in hex format + public key in hex format
 //
 // workMode: verify
 //second parameter -> hexdata that should be verified
 // third parameter -> signed data in hex format for verification
-// forth parameter -> public key for verification
+// forth parameter -> public key for verification or the path to a file (json/txt)
 //          output -> true (exitcode 0) or false (exitcode 1)
 async function main() {
 
-        //show help or usage if no parameter
-        if ( ! process.argv[2] || process.argv[2].toLowerCase().includes("help") ) { console.log(`${appname} ${version}`); showUsage(); }
+        //show help or usage if no parameter is provided
+        if ( ! process.argv[2] || process.argv[2].toLowerCase().includes('help') ) { console.log(`${appname} ${version}`); showUsage(); }
 
         //show version
-        if ( process.argv[2].toLowerCase().includes("version") ) { console.log(`${appname} ${version}`); process.exit(0); }
+        if ( process.argv[2].toLowerCase().includes('version') ) { console.log(`${appname} ${version}`); process.exit(0); }
 
         //first paramter - workMode: "sign or verify"
         var workMode = process.argv[2];
@@ -63,7 +135,7 @@ async function main() {
 	//choose the workmode
         switch (workMode) {
 
-                case "sign":  //SIGN DATA
+                case "sign":  //SIGN HEX DATA
 
 			//get data to sign -> store it in sign_data
 			var sign_data = process.argv[3];
@@ -74,12 +146,11 @@ async function main() {
 			if ( ! regExp.test(sign_data) ) { console.log(`Error: Data to sign is not a valid hex string`); showUsage(); }
 
 			//get signing key -> store it in sign_key
-			var sign_key = process.argv[4];
-		        if ( ! sign_key ) { console.log(`Error: Missing secret key (hex)`); showUsage(); }
-		        sign_key = trimString(sign_key.toLowerCase());
+			var key_file_hex = process.argv[4];
+		        if ( ! key_file_hex ) { console.log(`Error: Missing secret key parameter`); showUsage(); }
 
-			//check that the given key is a hex string
-			if ( ! regExp.test(sign_key) ) { console.log(`Error: Secret key is not a valid hex string`); showUsage(); }
+			//read in the key from a file or direct hex
+		        sign_key = readKey2hex(key_file_hex, 'secret');
 
 			//load the private key (normal or extended)
 			try {
@@ -102,7 +173,7 @@ async function main() {
 
 
 
-                case "verify":	//VERIFY DATA
+                case "verify":	//VERIFY HEX DATA
 
 			//get data to verify -> store it in verify_data
 			var verify_data = process.argv[3];
@@ -110,7 +181,7 @@ async function main() {
 		        verify_data = trimString(verify_data.toLowerCase());
 
 			//check that the given data is a hex string
-			if ( ! regExp.test(verify_data) ) { console.log(`Error: Data to verify is not a valid hex string`); showUsage(); }
+			if ( ! regExp.test(verify_data) ) { console.log(`Error: Data to verify is not a valid hex string`); process.exit(1); }
 
 			//get signed_data(signature) to verify -> store it in signed_data
 			var signed_data = process.argv[4];
@@ -118,15 +189,14 @@ async function main() {
 		        signed_data = trimString(signed_data.toLowerCase());
 
 			//check that the given signed_data is a hex string
-			if ( ! regExp.test(signed_data) ) { console.log(`Error: Signature(signed_data) is not a valid hex string`); showUsage(); }
+			if ( ! regExp.test(signed_data) ) { console.log(`Error: Signature(signed_data) is not a valid hex string`); process.exit(1); }
 
 			//get public key -> store it in public_key
-			var public_key = process.argv[5];
-		        if ( ! public_key ) { console.log(`Error: Missing public key (hex)`); showUsage(); }
-		        public_key = trimString(public_key.toLowerCase());
+			var key_file_hex = process.argv[5];
+		        if ( ! key_file_hex ) { console.log(`Error: Missing public key parameter`); showUsage(); }
 
-			//check that the given key is a hex string
-			if ( ! regExp.test(public_key) ) { console.log(`Error: Public key is not a valid hex string`); showUsage(); }
+			//read in the key from a file or direct hex
+		        public_key = readKey2hex(key_file_hex, 'public');
 
 			//load the public key
 			try {
@@ -144,11 +214,11 @@ async function main() {
 			//output the result and exit with the right exitcode
 			if ( verified ) { console.log(`true`); process.exit(0); }
 				   else { console.log(`false`); process.exit(1); }
-
 			break;
 
+
 		default:
-		        //if workMode is not found exit with showUsage
+		        //if workMode is not found, exit with and errormessage and showUsage
 			console.log(`Error: Unsupported command`);
 			showUsage();
 
